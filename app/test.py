d91eaf06d0c279d9c39838c54c7d4a48be499b66
WesselValcon <wessel.de.jong@valcon.com>
2023-10-30 18:54:55 +0100

    unittests integrated

--- app/test.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = (SparkSession.builder.master("local").appName("test_result_size").getOrCreate())
-
-print(spark.conf.get("spark.driver.maxResultSize"))
